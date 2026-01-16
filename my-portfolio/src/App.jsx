d64ff24a3c8017404719471f19c6625b3a6ff860
malesebuzuneh <malesebuzuneh@gmail.com>
2026-01-16 07:36:53 -0800

    Fix GitHub Pages routing with basename and 404 fallback

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename="/portfolio-by-React">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
